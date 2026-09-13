4f10040f56efaf8bd62e595293e330788aa9eda4
Parallel Studio <angie219@gmail.com>
2026-09-13 20:37:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Paste the Web App URL you get after deploying the Apps Script (Code.gs) here.
 // It looks like: https://script.google.com/macros/s/XXXXXXXXXXXX/exec
-const API_URL = "PASTE_YOUR_APPS_SCRIPT_URL_HERE";
+const API_URL = "1o-rN13gL-efFyLdPj6RHMeZktWz2EkF1XHyMTTN70TI";
